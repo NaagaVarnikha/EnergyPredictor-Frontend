1382e090c142a94b00a0bef79470fd8dd9458bc8
NaagaVarnikha <naagavarnikha@gmail.com>
2025-04-28 21:05:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function makePrediction() {
+    const country = document.getElementById("country").value;
+    const year = document.getElementById("year").value;
+
+    // Validate if year is a number
+    if (isNaN(year) || year.trim() === "") {
+        document.getElementById("predictionResult").innerText = "Please enter a valid year.";
+        return;
+    }
+    fetch('/predict', {
+        method: 'POST',  
+        headers: {
+            'Content-Type': 'application/json',  
+        },
+        body: JSON.stringify({ country: country, year: year })  
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.prediction) {
+            document.getElementById("predictionResult").innerText = `Predicted Energy Demand for ${country} in ${year}: ${data.prediction} TWh`;
+        } else {
+            document.getElementById("predictionResult").innerText = "Sorry, no data available for the selected parameters.";
+        }
+    })
+    .catch(error => {
+        document.getElementById("predictionResult").innerText = "An error occurred. Please try again later.";
+    });
+}
+
